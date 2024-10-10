5808b95f0872b0b41fb7f14c4396ee3d6382c396
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-10-10 16:32:17 +0300

    feat: Added the ability to receive videos from the static/videos

---
+++ controller/video.controller.js
@@ -0,0 +1,58 @@
+
+const fs = require('fs')
+const path = require('path')
+
+class VideoController {
+  async streamVideo(req, res) {
+    const { filename } = req.params; //get name file
+    const videoPath = path.join(__dirname, '../static/videos', filename); //create path to file
+
+    fs.stat(videoPath, (err, stats) => {
+      if (err || !stats) {
+        return res.status(404).send('Video not found')
+      }
+
+      const fileSize = stats.size
+      const range = req.headers.range// get range
+
+      if (range) {//if client get some range
+
+        const parts = range.replace(/bytes=/, "").split("-");// [bytes,bytes ]
+        const start = parseInt(parts[0], 10); // to number (ten formal)
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1; // if end bytes exist set ,if not set last bytes of file
+
+        const chunkSize = (end - start) + 1; // set chunk size
+
+        const file = fs.createReadStream(videoPath, { start, end });
+        //We create a stream for reading a video file with a given beginning and end(only part of the file is sent)
+
+        const head = {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`, // set range
+          'Accept-Ranges': 'bytes', //We inform you that the server supports requests in parts(byte ranges)
+          'Content-Length': chunkSize, // chunk size sended
+          'Content-Type': 'video/mp4', // type format
+        };
+
+        res.writeHead(206, head); // 206 (Partial Content)
+
+        file.pipe(res); // send part of video
+      } else { // get full file if not exist range
+        const head = {
+          'Content-Length': fileSize, // file size
+          'Content-Type': 'video/mp4', // type video mp4
+        };
+        res.writeHead(200, head);// send 200 ok ,send full file
+        fs.createReadStream(videoPath).pipe(res); // create a stream to read the entire file and send it to the client
+
+      }
+    }
+    )
+
+
+  }
+
+
+
+}
+
+module.exports = new VideoController()
--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const WS_PORT = process.env.WS_PORT || 3001
 const fileUpload = require('express-fileupload')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const jwt = require('jsonwebtoken')
-const db = require('./db')
 const bodyParser = require('body-parser')
 const path = require('path')
 const http = require('http');
@@ -21,6 +19,7 @@ const commentRouter = require('./routes/comment.routes')
 const voteRouter = require('./routes/vote.routes')
 const chatRouter = require('./routes/chat.routes')
 const subscriptionRouter = require('./routes/subscription.routes')
+const videoRouter = require('./routes/video.routes')
 
 
 dotenv.config();
@@ -29,7 +28,7 @@ const server = http.createServer(app);
 ws.initWebSocketServer(server); // init websocket
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}!`))
-server.listen(WS_PORT, () => console.log(`Server listening on port ${WS_PORT}!`))
+server.listen(WS_PORT, () => console.log(`Server ws listening on port ${WS_PORT}!`))
 
 app.use(express.json())
 app.use(cors());
@@ -39,34 +38,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, './static')));
 
 
-async function verifyToken(req, res, next) {
-  const token = req.headers.authorization && req.headers.authorization.split(' ')[1]
 
-  if (!token) {
-    return res.status(401).json({ message: "Missing token" })
-  }
 
-  try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
-    if (decoded) {
-      const result = await db.query('SELECT * FROM "usersReg" WHERE id = $1', [decoded.userId]);
-
-      if (result.rows.length > 0) {
-        req.user = result.rows[0];
-        next();
-      } else {
-        res.status(404).json({ message: "User not found" });
-      }
-    } else {
-      res.status(401).json({ message: "Invalid token" });
-    }
-  } catch (error) {
-    console.error('Token verification failed', error.message);
-    res.status(401).send('Invalid token');
-  }
-}
-
+app.use('/api', videoRouter)
 app.use('/api', authRouter)
 app.use('/api', verifyToken, postRouter)
 app.use('/api', verifyToken, profileRouter)
